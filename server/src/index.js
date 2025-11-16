68906b40b6c90fb00ba51a5749500b91991e868a
Kim Young Woo <85552569+hppYW@users.noreply.github.com>
2025-11-16 15:10:02 +0900

    dd

--- server/src/index.js
+++ server/src/index.js
@@ -14,7 +14,12 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:5174',
+    'http://localhost:5175',
+    process.env.CLIENT_URL
+  ].filter(Boolean),
   credentials: true
 }));
 app.use(express.json());
